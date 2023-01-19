064a6b36bdc8bd4ca992b13da6ee11f7e9478b38
baks.dev <admin@baks.dev>
2023-01-20 00:56:19 +0300

    Baks Development

--- Resources/config/twig.php
+++ Resources/config/twig.php
@@ -6,14 +6,7 @@ use Symfony\Config\TwigConfig;
 
 return static function (TwigConfig $config)
 {
-    
     $config->path(__DIR__.'/../view', 'AuthEmail');
-
-//    $config->global('AuthEmail')->value(true);
-//    $config->global('AuthEmailLogin')->value(true);
-//    $config->global('AuthEmailRegistration')->value(true);
-//    $config->global('AuthEmailRestore')->value(true);
-    
 };
 
 
