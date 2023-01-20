ff90d9649e8cc06f968a17846b59acfafd497003
baks.dev <admin@baks.dev>
2023-01-20 19:07:28 +0300

    Baks Development

--- Resources/config/doctrine.php
+++ Resources/config/doctrine.php
@@ -7,7 +7,7 @@ use BaksDev\Auth\Email\Type\Email\AccountEmail;
 use BaksDev\Auth\Email\Type\Email\AccountEmailType;
 use BaksDev\Auth\Email\Type\Event\AccountEventType;
 use BaksDev\Auth\Email\Type\Event\AccountEventUid;
-use BaksDev\Auth\Email\Type\Settings\AccountSettings;
+use BaksDev\Auth\Email\Type\Settings\AccountSettingsIdentifier;
 use BaksDev\Auth\Email\Type\Settings\AccountSettingsType;
 use BaksDev\Auth\Email\Type\Status\AccountStatus;
 use BaksDev\Auth\Email\Type\Status\AccountStatusType;
@@ -19,7 +19,7 @@ return static function (DoctrineConfig $doctrine)
     $doctrine->dbal()->type(AccountEventUid::TYPE)->class(AccountEventType::class);
     $doctrine->dbal()->type(AccountEmail::TYPE)->class(AccountEmailType::class);
     $doctrine->dbal()->type(AccountStatus::TYPE)->class(AccountStatusType::class);
-    $doctrine->dbal()->type(AccountSettings::TYPE)->class(AccountSettingsType::class);
+    $doctrine->dbal()->type(AccountSettingsIdentifier::TYPE)->class(AccountSettingsType::class);
 	
     $emDefault = $doctrine->orm()->entityManager('default');
     
