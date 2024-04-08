5addad47e2d2468a1b464ed428098f3238da9a40
kingstar <admin@baks.dev>
2024-04-08 18:08:57 +0300

    Baks Development

--- Controller/User/Restore/ResetController.php
+++ Controller/User/Restore/ResetController.php
@@ -11,7 +11,8 @@ use BaksDev\Core\Type\UidType\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\HttpKernel\UriSigner;
+//use Symfony\Component\HttpKernel\UriSigner;
+use Symfony\Component\HttpFoundation\UriSigner;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
