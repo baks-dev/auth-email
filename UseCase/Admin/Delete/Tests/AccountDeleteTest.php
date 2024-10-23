1820c5b570f6760ff304a273fe90b17465fd38b5
kingstar <admin@baks.dev>
2024-10-24 01:14:00 +0300

    Baks Development

--- UseCase/Admin/Delete/Tests/AccountDeleteTest.php
+++ UseCase/Admin/Delete/Tests/AccountDeleteTest.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,17 +29,12 @@ use BaksDev\Auth\Email\Entity\Account;
 use BaksDev\Auth\Email\Entity\Event\AccountEvent;
 use BaksDev\Auth\Email\Repository\UserAccountEvent\UserAccountEventInterface;
 use BaksDev\Auth\Email\Type\Email\AccountEmail;
-use BaksDev\Auth\Email\Type\EmailStatus\EmailStatus;
 use BaksDev\Auth\Email\Type\EmailStatus\Status\EmailStatusActive;
 use BaksDev\Auth\Email\Type\EmailStatus\Status\EmailStatusBlock;
-use BaksDev\Auth\Email\Type\EmailStatus\Status\EmailStatusNew;
-use BaksDev\Auth\Email\Type\Event\AccountEventUid;
 use BaksDev\Auth\Email\UseCase\Admin\Delete\AccountDeleteDTO;
 use BaksDev\Auth\Email\UseCase\Admin\Delete\AccountDeleteHandler;
 use BaksDev\Auth\Email\UseCase\Admin\NewEdit\AccountDTO;
-use BaksDev\Auth\Email\UseCase\Admin\NewEdit\AccountHandler;
 use BaksDev\Core\Doctrine\DBALQueryBuilder;
-use BaksDev\Core\Doctrine\ORMQueryBuilder;
 use BaksDev\Users\User\Entity\User;
 use BaksDev\Users\User\Type\Id\UserUid;
 use Doctrine\ORM\EntityManagerInterface;
