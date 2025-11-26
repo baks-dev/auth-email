5fa136d2a2e2e00524cf71054c9bbdc185381de5
MichelAngelo <baksdevelopmen@gmail.com>
2025-11-26 17:28:35 +0300

    BaksDevelopment
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Type/Email/AccountEmail.php
+++ Type/Email/AccountEmail.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -38,7 +38,7 @@ final class AccountEmail
 
         if(!empty($value) && !filter_var($value, FILTER_VALIDATE_EMAIL))
         {
-            throw new InvalidArgumentException('Incorrect Email.');
+            throw new InvalidArgumentException(sprintf('Incorrect Email %s', $value));
         }
 
         $this->value = mb_strtolower($value);
